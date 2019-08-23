3a77122b553043827b6b8ceaaf50571098fec572
Michael Williams <manicdesigner@gmail.com>
2019-08-22 22:45:16 -0400

    add div box arround numbers section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from './components/DisplayComponents/Logo';
 import Display from './components/DisplayComponents/Display';
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
@@ -19,6 +20,9 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Display />
+        <div className="box">
+          <Numbers />
+        </div>
       </div>
     </div>
   );
