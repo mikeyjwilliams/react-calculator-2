b97ad3ebd17004f46aadc87b224f48e5aac71c4c
Michael Williams <manicdesigner@gmail.com>
2019-08-23 19:00:26 -0400

    classname turned to back tick

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const NumberButton = props => {
-  const { number } = props;
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className="btn numbers-btn">{number}</button>
-    </>
-  );
+const NumberButton = (props) => {
+	const { number } = props;
+
+	const normalButtonClasses = `btn numbers-btn`;
+	return (
+		<>
+			{/* Display a button element rendering the data being passed down from the parent container on props */}
+			<button className={normalButtonClasses}>{number}</button>
+		</>
+	);
 };
 export default NumberButton;
