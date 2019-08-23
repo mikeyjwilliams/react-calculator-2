e0972b2e72f448ad0be99c9fc009aaf6e7d86e85
Michael Williams <manicdesigner@gmail.com>
2019-08-23 10:05:35 -0400

    change operators class to operators-btn class name

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -5,7 +5,7 @@ const OperatorButton = props => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className="btn operators">{operator}</button>
+      <button className="btn operators-btn">{operator}</button>
     </>
   );
 };
