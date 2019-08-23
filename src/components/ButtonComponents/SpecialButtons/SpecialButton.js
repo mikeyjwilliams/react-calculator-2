6c9a40e97fc407fe07691dbaab52cd46649b5e19
Michael Williams <manicdesigner@gmail.com>
2019-08-23 19:00:03 -0400

    format  specialbutton

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 const SpecialButton = (props) => {
 	const { special } = props;
+	const specialBaseClass = `btn specials-btn`;
 	return (
 		<>
 			{/* Display a button element rendering the data being passed down from the parent container on props */}
-			<button {(special === '0' ? null : `className="${btn}  ${specials - btn}"`)}>
-				{special}
-			</button>
+			<button className={specialBaseClass}>{special}</button>
 		</>
 	);
 };
