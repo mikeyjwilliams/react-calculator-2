c7860f5e698df9b7dfb05f301541f7c2a99198fa
Michael Williams <manicdesigner@gmail.com>
2019-08-23 18:31:14 -0400

    started to alter special === 0 change classnames

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const SpecialButton = props => {
-  const { special } = props;
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className="btn specials-btn">{special}</button>
-    </>
-  );
+const SpecialButton = (props) => {
+	const { special } = props;
+	return (
+		<>
+			{/* Display a button element rendering the data being passed down from the parent container on props */}
+			<button {(special === '0' ? null : `className="${btn}  ${specials - btn}"`)}>
+				{special}
+			</button>
+		</>
+	);
 };
 export default SpecialButton;
