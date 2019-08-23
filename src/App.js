25d8b010455c4771986747cca3d21f15d1c05c2f
Michael Williams <manicdesigner@gmail.com>
2019-08-23 10:03:06 -0400

    import Operators component add Item-container div pass in Operators component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Logo from './components/DisplayComponents/Logo';
 import Display from './components/DisplayComponents/Display';
 import Specials from './components/ButtonComponents/SpecialButtons/Specials';
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
+import Operators from './components/ButtonComponents/OperatorButtons/Operators';
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
@@ -21,12 +22,14 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Display />
-        <div className="box">
-          <Specials />
-          <Numbers />
+        <div className="item-container">
+          <div className="box">
+            <Specials />
+            <Numbers />
+          </div>
         </div>
         <div className="operations-side">
-          {/* operations component goes here */}
+          <Operators />
         </div>
       </div>
     </div>
