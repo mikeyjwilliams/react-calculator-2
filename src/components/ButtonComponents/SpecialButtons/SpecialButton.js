80d37d6ec593868c6d81afc404e0111fe92c6b43
Michael Williams <manicdesigner@gmail.com>
2019-08-23 23:23:48 -0400

    displaying special buttons as of now

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const SpecialButton = (props) => {
-	const { special } = props;
-	const specialBaseClass = `btn specials-btn`;
-	return (
-		<>
-			<button className={specialBaseClass}>{special}</button>
-		</>
-	);
+const SpecialButton = props => {
+  const { special, setDisplayState } = props;
+  const specialBaseClass = `btn specials-btn`;
+
+  const displaySpecialChar = () => {
+    setDisplayState(special);
+  };
+
+  return (
+    <>
+      <button onClick={displaySpecialChar} className={specialBaseClass}>
+        {special}
+      </button>
+    </>
+  );
 };
 export default SpecialButton;
