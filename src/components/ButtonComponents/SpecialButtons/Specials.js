165327aa9b4333c495b61693bb5f7ffe267d15f3
Michael Williams <manicdesigner@gmail.com>
2019-08-23 23:24:13 -0400

    prop drilling for setDisplayState

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -3,16 +3,22 @@ import React from 'react';
 import SpecialButton from './SpecialButton';
 
 import { specials } from '../../../data';
-const Specials = () => {
-	// STEP 2 - add the imported data to state
-
-	return (
-		<div>
-			{specials.map((special, index) => {
-				return <SpecialButton special={special} key={index} />;
-			})}
-		</div>
-	);
+const Specials = props => {
+  // STEP 2 - add the imported data to state
+  const { setDisplayState } = props;
+  return (
+    <div>
+      {specials.map((special, index) => {
+        return (
+          <SpecialButton
+            special={special}
+            key={index}
+            setDisplayState={setDisplayState}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Specials;
