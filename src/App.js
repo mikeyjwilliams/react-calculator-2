8934a9a3df53a1f0fec0df203ae7f5b82236d589
Michael Williams <manicdesigner@gmail.com>
2019-08-23 20:35:55 -0400

    add test useStates
    numberDisplay listener

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
@@ -16,14 +16,20 @@ function App() {
 	// the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
 	// Don't forget to pass the functions (and any additional data needed) to the components as props
 
-	//const [numberState, setNumberState] = useState(number);
+	const [displayState, setDisplayState] = useState(0);
+	const [numberState, setNumberState] = useState(0);
+
+	const numberDisplay = (strNumber) => {
+		const stringToNumber = Number(strNumber);
+		setNumberState(stringToNumber);
+	};
 
 	return (
 		<div className="container">
 			<Logo />
 			<div className="App">
 				{/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-				<Display />
+				<Display displayState={displayState} />
 				<div className="specials-numbers-wrapper">
 					<div className="specials-nums-container">
 						<Specials />
