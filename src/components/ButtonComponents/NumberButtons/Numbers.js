2d05c5ef14c233fd5df6c2b7278b2ea7c2523250
Michael Williams <manicdesigner@gmail.com>
2019-08-23 22:12:33 -0400

    numbers formatted

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-//import any components needed
 import NumberButton from './NumberButton';
-//Import your array data to from the provided data file
+
 import { numbers } from '../../../data';
 const Numbers = () => {
 	// STEP 2 - add the imported data to state
@@ -11,9 +10,6 @@ const Numbers = () => {
 			{numbers.map((number, index) => {
 				return <NumberButton number={number} key={index} />;
 			})}
-			{/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
 		</div>
 	);
 };
