2e5b8b58d43267727638f641729a7a1cf07cb60b
Michael Williams <manicdesigner@gmail.com>
2019-08-29 12:05:08 -0400

    make number buttons appear

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
-const NumberButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+const NumberButton = (props) => {
+	const { text } = props;
+	return (
+		<>
+			<button>{text}</button>
+		</>
+	);
 };
+export default NumberButton;
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 
-//import any components needed
 import NumberButton from './NumberButton';
-//Import your array data to from the provided data file
+
 import { numbers } from '../../../data';
 const Numbers = () => {
 	const [numberData, setNumberData] = useState(numbers);
 	return (
 		<div>
 			{numberData.map((numButton) => {
-				return <NumberButton numButton={numButton} />;
+				return <NumberButton text={numButton} />;
 			})}
 		</div>
 	);
 };
+export default Numbers;
