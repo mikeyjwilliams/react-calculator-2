e4ddc7df402b1ee11d9ad2270171eac9893790db
Michael Williams <manicdesigner@gmail.com>
2019-08-29 12:05:21 -0400

    make special buttons appear

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from 'react';
 
-const SpecialButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+const SpecialButton = (props) => {
+	const { specialBtn } = props;
+	return (
+		<>
+			<button>{specialBtn}</button>
+		</>
+	);
 };
+
+export default SpecialButton;
